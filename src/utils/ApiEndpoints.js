1e386fb83256e1a646d57313a2c5de377de69d62
rk10062003 <ramakrishna.g@blunetworld.com>
2026-04-17 10:36:20 +0530

    deploy finised

--- src/utils/ApiEndpoints.js
+++ src/utils/ApiEndpoints.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://localhost:8080/api/v1.0/";
+export const BASE_URL = "https://moneymanager-backend-xa6f.onrender.com/api/v1.0/";
 const CLOUDINARY_CLOUD_NAME = "dhoy7ezfk"
 
 export const API_ENDPOINTS = {
